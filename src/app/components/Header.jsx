af26c9fb781ba7006ddda010e8d3bfa995e289ef
DebiiG Dev <deby.guardado@gmail.com>
2024-03-14 13:20:29 -0600

    Nuevo diseno pagina sorteos

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -14,11 +14,11 @@ const Header = () => {
                 <Link href={"/"} className="font-medium hover:text-gray-900">Contact</Link>
             </nav>
             <div className="items-center h-full">
-                <Link className="mr-5 font-medium hover:text-gray-900" href="/login">Login</Link>
-                <a href="#_"
+                <Link className="mr-5 font-medium hover:text-gray-900" href="/login"> Sign Up </Link>
+                <Link href="/login"
                     className="px-4 py-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-purple-500 rounded shadow outline-none active:bg-purple-600 hover:shadow-md focus:outline-none ease">
-                    Sign Up
-                </a>
+                   Login
+                </Link>
             </div>
         </div>
     </header>
