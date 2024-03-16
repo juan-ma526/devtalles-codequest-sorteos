3081076487124ae2d633f9b59eb91f00be57e27d
juan-ma526 <jmperez675@gmail.com>
2024-03-16 12:33:01 -0300

    terminada pagina success

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -1,4 +1,4 @@
-"use client";
+/* "use client";
 
 import { useContext } from "react";
 import { UserContext } from "../context/userContext";
@@ -16,4 +16,18 @@ export default function SuccesPage() {
     </>
   );
   return <div>{user ? renderUser() : renderDontUser()}</div>;
+
+ */
+import guitar from "../assets/guitar.png";
+import { CardResponse } from "../components/CardResponse";
+
+export const metadata = {
+  title: "Succes Page",
+  description: "Pagina para informar al usuario el registro exitoso",
+};
+
+export default function SuccesPage() {
+  return (
+    <CardResponse image={guitar} title="¡Gracias por participar en el sorteo!" response="Tu Registro fue Exitoso" />
+  );
 }
