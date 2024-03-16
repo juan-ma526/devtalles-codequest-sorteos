61629071aa3b433e4ab415192a5c29118aee01fa
juan-ma526 <jmperez675@gmail.com>
2024-03-16 12:33:50 -0300

    puesto 1 solo sorteo

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,18 @@
 import Sorteos from "./components/Sorteos";
 
 export default function Home() {
-
   const sorteos = [
-   {
-    id: 1,
-    title: "Sorteo 1",
-    description: "Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.",
-    createdAt: "",
-   },
-   {
-    id: 2,
-    title: "Sorteo 2",
-    description: "Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.",
-    createdAt: "",
-   }
-
-  ]
+    {
+      id: 1,
+      title: "Sorteo 1",
+      description:
+        "Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.",
+      createdAt: "",
+    },
+  ];
   return (
     <>
-     <Sorteos sorteos={sorteos} />
+      <Sorteos sorteos={sorteos} />
     </>
   );
 }
