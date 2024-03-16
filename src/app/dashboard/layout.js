c8e7b33d8d3814e9e0a4a0731d4a981b83bbf705
juan-ma526 <jmperez675@gmail.com>
2024-03-16 20:33:51 -0300

    add fix

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,17 +1,14 @@
-import Menu from "./components/Menu"
+import Menu from "./components/Menu";
 
-export default function layout({children}) {
+export default function layout({ children }) {
   return (
     <>
-
-            <div className="flex bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-purple-600 selection:text-white">
-                <div className="flex relative w-screen">
-                    <Menu />
-                    <div className="text-black px-2 mt-2 w-full">
-                        {children}
-                    </div>
-                </div>
-            </div>
-        </>
-  )
+      <div className="flex bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-purple-600 selection:text-white">
+        <div className="flex relative w-screen">
+          <Menu />
+          <div className="text-black px-2 mt-2 w-full">{children}</div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,30 +1,30 @@
-"use client"
 import { Suspense, useContext } from "react";
 import ListaSorteos from "./components/ListaSorteos";
 import Loading from "../loading";
 import { Page404 } from "../components/Page404";
-import { UserContext } from "../context/userContext";
 
 async function getSorteosList() {
-  const res = await fetch(`${process.env.BACKEND_URL}/api/sorteo`, { cache: 'no-store' });
+  const res = await fetch(`http://localhost:3000/api/sorteo`, { cache: "no-store" });
 
   if (!res.ok) {
-    throw new Error("Failed to fetch data")
+    throw new Error("Failed to fetch data");
   }
 
   return res.json();
 }
 
 export default async function Dashboard() {
-  const { user } = useContext(UserContext);
   const sorteos = await getSorteosList();
   const renderDontUser = () => <Page404 />;
-  return <div>{user ? <>
-    <Suspense fallback={<Loading />}>
-      <div className="w-full">
-        <ListaSorteos sorteos={sorteos.sorteoData} />
-      </div>
-    </Suspense>
-  </> : renderDontUser()}</div>;
+  return (
+    <div>
+      <>
+        <Suspense fallback={<Loading />}>
+          <div className="w-full">
+            <ListaSorteos sorteos={sorteos.sorteoData} />
+          </div>
+        </Suspense>
+      </>
+    </div>
+  );
 }
-
