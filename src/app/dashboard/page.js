3690574cec9e01bbdce47103d16424d394d3a3b8
DebiiG Dev <deby.guardado@gmail.com>
2024-03-16 14:54:17 -0600

    Loading funcionality and validate is the sort has participants

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,22 +1,26 @@
+import { Suspense } from "react";
 import ListaSorteos from "./components/ListaSorteos";
+import Loading from "../loading";
 
 async function getSorteosList() {
-    const res = await fetch(`http://localhost:3000/api/sorteo`, { cache: 'no-store' });
-  
-    if (!res.ok) {
-      throw new Error("Failed to fetch data")
-    }
-  
-    return res.json();
+  const res = await fetch(`http://localhost:3000/api/sorteo`, { cache: 'no-store' });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data")
+  }
+
+  return res.json();
 }
 
 export default async function Dashboard() {
-    const sorteos = await getSorteosList();
-    return (
-        <>
-            <div className="w-full">
-              <ListaSorteos sorteos={sorteos.sorteoData} />
-            </div>
-        </>
-    );
+  const sorteos = await getSorteosList();
+  return (
+    <>
+      <Suspense fallback={<Loading />}>
+        <div className="w-full">
+          <ListaSorteos sorteos={sorteos.sorteoData} />
+        </div>
+      </Suspense>
+    </>
+  );
 }
